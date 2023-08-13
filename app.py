709642aa303111f67ffac35f4588f12e48423512
Florenz Elvin <z5366991@ad.unsw.edu.au>
2023-08-13 10:03:33 +1000

    Created simple website with bootstrap

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print("Hello, World!")
+from flask import Flask, render_template
+
+APP = Flask(__name__)
+
+@APP.route("/")
+def hello_world():
+    return render_template('home.html')
+
+if __name__ == "__main__":
+    APP.run(host='0.0.0.0', debug=True) # Do not edit this port
+ 
